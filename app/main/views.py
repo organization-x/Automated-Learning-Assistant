8e64818905cb78b9cac5e0fef75e0cb215f359b0
Ayush1124 <97317706+Ayush1124@users.noreply.github.com>
2022-09-28 21:54:38 -0700

    django stuff

---
+++ app/main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import ToDo, Item
+
+# Create your views here.
+
+def home(response):
+    return render(response, 'home.html')
+
+def search(response):
+    return render(response, 'index.html')
+
+def about(response):
+    return render(response, 'aboutUs.html')
+
+def results(response):
+    return render(response, 'home.html')
