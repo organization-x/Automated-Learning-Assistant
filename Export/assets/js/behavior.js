2bcb8e979a565c998d13668e936856bff6d1ec3c
Paul Li <shi.li.314159@gmail.com>
2022-09-29 19:15:15 -0700

    Nice Autotype

--- Export/assets/js/behavior.js
+++ Export/assets/js/behavior.js
@@ -1,4 +1,4 @@
-function get () {
+function get() {
     // Set up parameter calling
     var allParams = new URLSearchParams(window.location.search), query;
     
@@ -7,4 +7,29 @@ function get () {
     return query;
 }
 
-console.log(get())
+function setSearchBarValue(){
+    // Get Search form value
+    var searchBar = document.forms['searchBar_Form']['query'];
+    console.log(searchBar.value);
+    searchBar.setAttribute('value',get());
+}
+
+console.log(get());
+setSearchBarValue();
+
+/*
+$.ajax({
+            type: "post",
+            url: window.location.href,
+            cache: false,
+            async: 'asynchronous',
+            dataType: 'html',
+            data: get(),
+            success: function(data) {
+                console.log(data)
+            },
+            error: function(request, status, error){
+                console.log("Error: " + error)
+            }
+        })
+*/
