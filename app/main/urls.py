875f4fa07301fed2acac1bebd936457cb79fa99d
Ayush1124 <97317706+Ayush1124@users.noreply.github.com>
2022-10-01 15:03:27 -0700

    transferred variables between views

--- app/main/urls.py
+++ app/main/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("aboutUs/", views.about, name='about'),
     path("", views.search, name="home"),
     path("resultPage/", views.results, name="results"),
-    path("search/", views.search, name="search")
+    path("search/", views.search, name="search"),
+    path("query/", views.query, name="query")
 ]
--- app/main/views.py
+++ app/main/views.py
@@ -1,20 +1,23 @@
 from django.shortcuts import render
 from django import forms
-from django.http import HttpRequest
+from django.http import HttpResponse
 
 # Create your views here.
+
+responses = HttpResponse()
 
 def about(response):
     return render(response, 'aboutUs.html')
 
 def results(response):
-    return render(response, 'result.html')
+    query = responses.get('query')
+    return render(response, 'result.html', {'query': query})
 
 def search(response):
     return render(response, 'index.html')
 
 def query(request):
     if request.method == 'POST':
-        q = request.POST['query']
+        q = str(request.POST['query'])
         print(q)
-        return render(request, 'result.html', {'query': q})
+        responses.headers['query'] = q
