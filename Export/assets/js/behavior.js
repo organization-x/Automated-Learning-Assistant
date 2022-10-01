42b3467715bdbe3a604efe92768ed9db5801c53f
Paul Li <shi.li.314159@gmail.com>
2022-09-30 20:18:55 -0700

    Export refresh

---
+++ Export/assets/js/behavior.js
@@ -0,0 +1,37 @@
+function get() {
+    // Set up parameter calling
+    var allParams = new URLSearchParams(window.location.search);
+    var query;
+    // Retrieve parameter name
+    query = allParams.get("query");
+    return query;
+}
+
+function setSearchBarValue(){
+    // Get Search form value
+    var searchBar = document.getElementById('searchText');
+    // console.log(searchBar.name);
+    if (searchBar.name == "result"){
+        searchBar.setAttribute('value',get());
+    }
+}
+
+//console.log(get());
+setSearchBarValue();
+
+/*
+$.ajax({
+            type: "post",
+            url: window.location.href,
+            cache: false,
+            async: 'asynchronous',
+            dataType: 'html',
+            data: get(),
+            success: function(data) {
+                console.log(data)
+            },
+            error: function(request, status, error){
+                console.log("Error: " + error)
+            }
+        })
+*/
