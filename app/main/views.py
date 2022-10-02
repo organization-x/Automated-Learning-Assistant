026c97192080c69b1b1f08e1b2a0391dc951cc96
Ayush1124 <97317706+Ayush1124@users.noreply.github.com>
2022-10-01 20:57:24 -0700

    roadmap gpt3 added

--- app/main/views.py
+++ app/main/views.py
@@ -13,17 +13,27 @@ def about(response):
 
 def results(response):
     query = responses.get('query')
-    p = f"Explain in informative terms to a non programmer in 300 words. {query}"
+    p1 = f"Explain in informative terms to a non programmer in 300 words. {query}"
+    p2 = f"Give a roadmap that is a series of instructions that someone should take to solve this question. {query} Include line breaks after each point."
     resp = openai.Completion.create(
         model="text-davinci-002",
-        prompt= p,
+        prompt= p1,
         temperature=0.7,
         max_tokens=500,
         top_p=1,
         frequency_penalty=0,
         presence_penalty=0
 )
-    resps = {'response': resp["choices"][0]["text"], 'query': query}
+    roadmap = openai.Completion.create(
+        model="text-davinci-002",
+        prompt= p2,
+        temperature=0.7,
+        max_tokens=500,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    resps = {'response': resp["choices"][0]["text"], 'query': query, 'roadmap': roadmap["choices"][0]["text"]}
     return render(response, 'result.html', resps)
 
 def search(response):
@@ -31,6 +41,7 @@ def search(response):
 
 def query(request):
     if request.method == 'POST':
-        q = str(request.POST['query'])
-        print(q)
-        responses.headers['query'] = q
+        if 'query' in request.POST:
+            q = str(request.POST['query'])
+            print(q)
+            responses.headers['query'] = q
