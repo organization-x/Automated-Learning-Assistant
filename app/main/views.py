4bf23bcf1ab00b66fc047576a34b99a766d7a423
Paul Li <shi.li.314159@gmail.com>
2022-10-03 18:05:17 -0700

    Updated for AioHttp

--- app/main/views.py
+++ app/main/views.py
@@ -1,42 +1,69 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import openai
+import asyncio
+import aiohttp
+import json
+from dotenv import load_dotenv
+import os
+import time
 
-# Create your views here.
-
-openai.api_key = "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"
-
+load_dotenv()
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+set_api_key = os.getenv('OPENAI_API_KEY')
 responses = HttpResponse()
+
+
+def getPrompts(searchQuery):
+    p1 = f"Explain in informative terms to a non programmer in 300 words. {searchQuery}"
+    p2 = f"Give a roadmap that is a series of instructions that someone should take to solve this question. {searchQuery} Include line breaks after each point. "
+    p3 = f"Return a link with a summary of well formatted code that solves this problem: {searchQuery}"
+    prompts = []
+    explanation = {
+        'prompt': p1,
+        'temperature': 0.7,
+        'max_tokens': 500,
+        'top_p': 1,
+        'frequency_penalty': 0,
+        'presence_penalty': 0
+    }
+    roadmap = {
+        'prompt': p2,
+        'temperature': 0.7,
+        'max_tokens': 500,
+        'top_p': 1,
+        'frequency_penalty': 0,
+        'presence_penalty': 0
+    }
+    prompts.append(explanation)
+    prompts.append(roadmap)
+    return prompts
+
+
+async def get_text(session, url, params):
+    async with session.post(url, json=params) as resp:
+        text = await resp.json()
+        return text['choices'][0]['text']
+
+
+async def resultsAsync(searchQuery):
+    prompts = getPrompts(searchQuery)
+    async with aiohttp.ClientSession(headers={'authorization': f"Bearer {set_api_key}"}) as session:
+        tasks = []
+        for prompt in prompts:
+            url = 'https://api.openai.com/v1/engines/text-davinci-002/completions'
+            tasks.append(asyncio.ensure_future(get_text(session, url, prompt)))
+        feedbacks = await asyncio.gather(*tasks)
+    return {'response': feedbacks[0], 'query': searchQuery, 'roadmap': feedbacks[1]}
+
+
+def results(response):
+    searchQuery = responses.get('query')
+    numResults = 2
+    resps = asyncio.run(resultsAsync(searchQuery))
+    return render(response, 'result.html', resps)
 
 def about(response):
     return render(response, 'aboutUs.html')
-
-def results(response):
-    query = responses.get('query')
-    print(query)
-    p1 = f"Explain  to a non programmer in at least 300 words.{query}"
-    p2 = f"Give a roadmap that is a series of instructions that someone should take to solve this question. {query} Number each point"
-    
-    resp = openai.Completion.create(
-        model="text-davinci-002",
-        prompt= p1,
-        temperature=0.15,
-        max_tokens=500,
-        top_p=0.5,
-        frequency_penalty=0,
-        presence_penalty=0.5
-    )
-    roadmap = openai.Completion.create(
-        model="text-davinci-002",
-        prompt= p2,
-        temperature=0.7,
-        max_tokens=500,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    resps = {'response': resp["choices"][0]["text"], 'query': query, 'roadmap': roadmap["choices"][0]["text"]}
-    return render(response, 'result.html', resps)
 
 def search(response):
     return render(response, 'index.html')
