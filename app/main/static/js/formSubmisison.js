985a351f22f3495c1c5072ae83ac91c2be6d3553
Paul Li <shi.li.314159@gmail.com>
2022-10-13 20:38:56 -0700

    Reorganized files

---
+++ app/main/static/js/formSubmisison.js
@@ -0,0 +1,76 @@
+// Default Vars
+var searchQuery, redirectLink="/resultPage";
+// Ajax Vars
+URL = "/calls";
+
+// Function executed whwn submitting the form
+function execute(){
+    // Get User Query
+    var searchBar = document.getElementById('searchText');
+    searchQuery = searchBar.value;
+    // Printing stuff for debug
+    query = {'query': searchQuery};
+    $.post("/query/", query);
+    // Redirect URL
+    changeURL("/resultPage", searchQuery, true);
+}
+// Event Listeners
+function submitButton() {
+    var buttonPress = document.getElementById("searchButton");
+    buttonPress.addEventListener("click", function(event) {
+        execute();
+        event.preventDefault();
+    });
+}
+
+function submitEnterKey() {
+    var submittedQuery = document.getElementById("searchText");
+    submittedQuery.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            execute();
+            event.preventDefault();
+        }
+    });
+}
+
+submitEnterKey()
+submitButton()
+
+// Pass search info
+
+function search2Param(query) {
+    return encodeURI(query);
+}
+
+function search2Param(query) {
+    return encodeURI(query);
+}
+
+function getURL() {
+    return window.location.href;
+}
+
+function splitOnLast(rawString, splitStr) {
+    const lastIndex = rawString.lastIndexOf(splitStr);
+    return rawString.slice(0, lastIndex);
+} 
+
+function giveURL(localPage, params="", redirect=false) {
+    params = "?query=" + params;
+    if (redirect == false){
+        return splitOnLast(document.location.href, '/')+localPage+params;
+    }
+    else{
+        return localPage+params;
+    }
+}
+
+function changeURL(localPage, params="", redirect=false) {
+    params = "?query=" + params;
+    if (redirect == false){
+        window.location.href = splitOnLast(document.location.href, '/')+localPage+params;
+    }
+    else{
+        window.location.href = localPage+params;
+    }
+}
