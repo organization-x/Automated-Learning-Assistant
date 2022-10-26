5b673a985bb56eda145e6b3e273f94395ba47789
Ayush1124 <97317706+Ayush1124@users.noreply.github.com>
2022-10-25 17:10:24 -0700

    validation doesn't work

--- app/main/bulk.py
+++ app/main/bulk.py
@@ -18,7 +18,6 @@ from search_engine_parser.core.engines.yahoo import Search as YahooSearch
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 # initializes pyenchant object
-text_validator = enchant.Dict("en_US")
 load_dotenv()
 #asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 nest_asyncio.apply()
@@ -30,9 +29,10 @@ SENTENCES_COUNT = 10
 #Get prompts for GPT-3
 def get_prompts(searchQuery):
     #Aryaman's code for text validation(Remove this later)
-    valid_text = text_validator.check(searchQuery)
-    if (valid_text == False):
-        print("Error: Text is not valid.")
+    # text_validator = enchant.Dict("en_US")
+    # valid_text = text_validator.check(searchQuery)
+    # if (valid_text == False):
+    #     print("Error: Text is not valid.")
 
     p1 = f"Explain in informative terms to a non programmer in 300 words. {searchQuery}"
     p2 = f"Give a roadmap that is a series of 5 steps that someone should take to solve this question. {searchQuery} The steps should be numbered as such: 1. First step, 2. Second step, 3. Third step, 4. Fourth step, 5. Fifth step."
